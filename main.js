402bc7c34c41622868343bbb94e1a5552f4fc98d
gkasianenko <85459917+gkasianenko@users.noreply.github.com>
2021-09-20 22:28:38 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,59 +1,38 @@
-const colorsSelector = document.querySelector("#colorsSelector");
-let colorsItems = document.querySelectorAll(".colorItem");
-const carsItems = document.querySelectorAll(".car-item");
-
-colorsSelector.addEventListener("click", changeColor);
-
-function changeColor(event) {
-
-    //Функция выделения выбранного цвета кружком
-    if(event.target.classList.contains("colorItem")) {
-       colorsItems.forEach(function(item){
-            item.classList.remove("colorItem--active");
-        });
-
-    //Выделить цель клика кружком
-    event.target.classList.add("colorItem--active");
-
-    //Находим цвета машин и вносим в массив
+//Алгоритм
 
 
-        carsItems.forEach(function(item) {
+//1. Найти на странице участвующие элементы во взаимодействии
 
-            item.classList.add("none");
+const carImage = document.querySelector("#car-img");
 
-            if(event.target.classList.contains("colorWhite") && item.getAttribute("data-color") == "colorWhite"){
-                item.style.display = "inline";
+const colorItems = document.querySelectorAll(".colorItem");
 
-            } 
-            else if(event.target.classList.contains("colorRed") && item.getAttribute("data-color") == "colorRed") {
-                item.style.display = "inline";
-                
-            } 
-            else if(event.target.classList.contains("colorGraphite") && item.getAttribute("data-color") == "colorGraphite") {
-                item.style.display = "inline";
-               
-            }
-            else if(event.target.classList.contains("colorWhitePure") && item.getAttribute("data-color") == "colorWhitePure") {
-                item.style.display = "inline";
-                
-            }
-            else if(event.target.classList.contains("colorBlack") && item.getAttribute("data-color") == "colorBlack") {
-                item.style.display = "inline";
-               
-            }
-            else if(event.target.classList.contains("colorOrange") && item.getAttribute("data-color") == "colorOrange") {
-                item.style.display = "inline";
-            }
-            else if(event.target.classList.contains("colorBlue") && item.getAttribute("data-color") == "colorBlue") {
-                item.style.display = "inline";
+//2. Отследить события пользователя
 
-            } else {
-                item.style.display = "none";
-            }
+colorItems.forEach(function(item) {
+    item.addEventListener("click", function(event){
+        
+        //3. Функции, которые сработают после пользовательских событий
+    
+        //Находим значение аттрибута data-file у кружочков
+        const fileName = event.target.dataset.file;
+      
+      //Назначаем новую строку src картинке авто через конкатенацию
+        carImage.src = `https://webcademy.ru/files/js2020/solaris/${fileName}`;
 
-        });
+        //4. Делаем кнопку по клику активной
+        const prevActiveButton = document.querySelector(".colorItem--active");
 
-    }
+        prevActiveButton.classList.remove("colorItem--active");
 
-};
+        event.target.classList.add("colorItem--active");
+
+
+        
+
+    });
+});
+
+
+
+
